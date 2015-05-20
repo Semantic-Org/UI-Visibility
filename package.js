2ddd06f083eb6003725041484fc21278a96d85b8
Pusher Robot <jack@semantic-ui.com>
2015-05-20 11:59:05 -0400

    Updated component to version 1.12.3

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-visibility',
   summary : 'Semantic UI - Visibility: Single component release',
-  version : '1.12.2',
+  version : '1.12.3',
   git     : 'git://github.com/Semantic-Org/UI-Visibility.git',
 });
 
